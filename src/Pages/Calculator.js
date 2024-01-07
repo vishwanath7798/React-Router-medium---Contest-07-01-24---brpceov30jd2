22b07cbe0e935f4322e9bab6d90db3d23e6339c0
vishwanath s patil <126999436+vishwanath7798@users.noreply.github.com>
2024-01-07 11:01:36 +0530

    Update Calculator.js

--- src/Pages/Calculator.js
+++ src/Pages/Calculator.js
@@ -1,9 +1,28 @@
-import React from "react";
+import React from 'react';
+import { useLocation } from 'react-router-dom';
 
+export const Calculator = () => {
+  const { search } = useLocation();
+  const params = new URLSearchParams(search);
+  const num1 = parseFloat(params.get('num1'));
+  const num2 = parseFloat(params.get('num2'));
+  const op = decodeURIComponent(params.get('op'));
 
-export const Calculator = () =>{
+  let result;
 
-    return(
-        
-    )
-}
+  switch (op) {
+    case '+':
+      result = num1 + num2;
+      break;
+    // Add more cases for other operations if needed
+
+    default:
+      result = 'Invalid operation';
+  }
+
+  return (
+    <div>
+      <h1>Your calculation Result is {result}</h1>
+    </div>
+  );
+};
