92b2973a555fb041c7e097f2cce8faad351afe2a
vishwanath s patil <126999436+vishwanath7798@users.noreply.github.com>
2024-01-07 11:01:10 +0530

    Update BackgroundColorChanger.js

--- src/Pages/BackgroundColorChanger.js
+++ src/Pages/BackgroundColorChanger.js
@@ -1,8 +1,24 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
+export const BackgroundColorChanger = () => {
+  const { colorname } = useParams();
 
-export const BackgroundColorChanger = () =>{
-    return(
-       
-    )
-}
+  const divStyle = {
+    height: '100vh',
+    backgroundColor: colorname,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    color: 'white',
+  };
+
+  return (
+    <div style={divStyle}>
+      <main>
+        <h1>Background Color Changer</h1>
+        <p>Color: {colorname}</p>
+      </main>
+    </div>
+  );
+};
